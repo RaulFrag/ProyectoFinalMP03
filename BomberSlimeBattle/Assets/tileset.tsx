30d6ddae0aa439d3a00fd1876e1fe07edb17dd79
ArnauV <arnau.vineta@epiagranollers.cat>
2025-05-15 17:16:28 +0200

    mapa1 actualizado

--- BomberSlimeBattle/Assets/tileset.tsx
+++ BomberSlimeBattle/Assets/tileset.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="tileset" tilewidth="16" tileheight="16" tilecount="153" columns="17" tilerendersize="grid">
- <image source="tileset.png" width="272" height="144"/>
+<tileset version="1.10" tiledversion="1.11.0" name="tileset" tilewidth="32" tileheight="32" tilecount="612" columns="17" tilerendersize="grid">
+ <image source="tileset.png" width="544" height="288"/>
 </tileset>
