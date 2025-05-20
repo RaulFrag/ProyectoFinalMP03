06534e594dc9387b6cca479a01b0d76b6a2d141c
RaulFrag <raul.fragoso@epiagranollers.cat>
2025-05-20 12:45:51 +0200

    Avance en las bombas

--- BomberSlimeBattle/Jugador.h
+++ BomberSlimeBattle/Jugador.h
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 #pragma once
 #include "ResourceManager.h"
 #include "Video.h"
@@ -22,3 +23,41 @@ public:
 	void render();
 };
 
+=======
+#pragma once
+#include "ResourceManager.h"
+#include "Video.h"
+#include "Bomb.h"
+
+class Jugador
+{
+	ResourceManager* rm = ResourceManager::getInstance();
+	Video* vid = Video::getInstance();
+	
+	SDL_Rect playerPos, playerSprite;
+
+	int playerId;
+
+	bool collision(std::vector <int> Layer2, std::vector <int> Layer3, int val, int xy);
+
+	std::vector<Bomb> bombas;
+
+public:
+	Jugador();
+	~Jugador();
+
+	int vel;
+
+	int checkKey();
+
+	void init(int tile, int id);
+	void loadSprite(std::string file);
+	void update(std::vector <int> Layer2, std::vector <int> Layer3, int key);
+	void render();
+
+	void placeBomb();
+	void updateBombs(std::vector<int> layer3, int mapWidth, int mapHeight);
+	void renderBombs();
+};
+
+>>>>>>> Stashed changes
